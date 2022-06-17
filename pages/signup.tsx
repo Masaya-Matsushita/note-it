10b45d361150041719895e39b3055495137ce38f
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-18 08:37:18 +0900

    ✨ mantine formでSignUpを書き換え

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,43 +1,69 @@
 import { NextPage } from 'next'
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
 import { app } from 'firebaseConfig/firebase'
-import { ChangeEventHandler, ComponentProps, useState } from 'react'
+import { z } from 'zod'
+import { useForm, zodResolver } from '@mantine/form'
+import { TextInput, Button, Box, Group, PasswordInput } from '@mantine/core'
+import { useRouter } from 'next/router'
+
+const schema = z.object({
+  email: z
+    .string()
+    .trim()
+    .email({ message: 'メールアドレスが正しくありません。' }),
+  password: z
+    .string()
+    .trim()
+    .regex(/^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i, {
+      message: '「半角英数をそれぞれ含む6文字以上」で入力してください。',
+    }),
+})
 
 const SignUp: NextPage = () => {
   const auth = getAuth(app)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const router = useRouter()
 
-  const handleSubmit: ComponentProps<'form'>['onSubmit'] = async (e) => {
-    e.preventDefault()
-    await createUserWithEmailAndPassword(auth, email, password)
-  }
+  const form = useForm({
+    schema: zodResolver(schema),
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  })
 
-  const handleChangeEmail: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setEmail(e.target.value)
-  }
-
-  const handleChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setPassword(e.target.value)
+  const handleSubmit = async (values: { email: string; password: string }) => {
+    console.log(values)
+    await createUserWithEmailAndPassword(auth, values.email, values.password)
+    router.push('/')
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type='text'
-          placeholder='メールアドレス'
-          value={email}
-          onChange={handleChangeEmail}
-        />
-        <input
-          type='text'
-          placeholder='パスワード'
-          value={password}
-          onChange={handleChangePassword}
-        />
-        <button>登録</button>
-      </form>
+      <Box sx={{ maxWidth: 480 }} mx='auto'>
+        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+          <TextInput
+            required
+            label='Email'
+            placeholder='example@mail.com'
+            size='md'
+            {...form.getInputProps('email')}
+          />
+          <PasswordInput
+            required
+            label='Password'
+            placeholder='半角英数6文字以上'
+            mt='sm'
+            size='md'
+            {...form.getInputProps('password')}
+          />
+
+          <Group position='right' mt='xl'>
+            <Button type='submit' size='md'>
+              登録
+            </Button>
+          </Group>
+        </form>
+      </Box>
     </div>
   )
 }
