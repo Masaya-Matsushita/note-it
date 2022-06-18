841da8866b5647382daf8ef67cba358c48cb9b54
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-18 13:26:50 +0900

    ✨ サインアウト処理を実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import { onAuthStateChanged } from 'firebase/auth'
+import { Button } from '@mantine/core'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from 'firebaseConfig/firebase'
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
   const checkAuthState = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -19,7 +23,12 @@ const Home: NextPage = () => {
     checkAuthState()
   }, [])
 
-  return <div>hello</div>
+  const signout = async () => {
+    await signOut(auth)
+    router.push('/signin')
+  }
+
+  return <Button onClick={signout}>サインアウト</Button>
 }
 
 export default Home
