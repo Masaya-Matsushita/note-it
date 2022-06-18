25d7367861eaa4bb73acb9771dd543e2b04db12c
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-18 13:08:47 +0900

    ✨ 認証状態オブザーバーでユーザーデータを取得

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import { Button } from '@mantine/core'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from 'firebaseConfig/firebase'
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <div>Hello World!</div>
-      <Button>btn</Button>
-    </div>
-  )
+  const checkAuthState = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid
+        console.log(uid)
+      } else {
+        console.log('User is signed out!')
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkAuthState()
+  }, [])
+
+  return <div>hello</div>
 }
 
 export default Home
