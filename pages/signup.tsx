b6a3900e306575f7f0270fb80e7cb9bbca251c92
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-18 23:13:19 +0900

    ✨ facebook、twitterログインUIを実装

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,16 +3,18 @@ import { auth } from 'firebaseConfig/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { TextInput, Button, Box, Group, PasswordInput } from '@mantine/core'
 import { AiOutlineDatabase, AiOutlineKey, AiOutlineMail } from 'react-icons/ai'
-import { FcGoogle } from 'react-icons/fc'
 import { useRouter } from 'next/router'
 import { useAuthFormInitialized } from 'hooks/useAuthFormInitialized'
-import { useGoogleSignIn } from 'hooks/useGoogleSignIn'
+import { useAuthThirdParty } from 'hooks/useAuthThirdParty'
 import { useEffect } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const SignUp: NextPage = () => {
   const router = useRouter()
   const form = useAuthFormInitialized()
-  const { googleSignIn, redirectToTop } = useGoogleSignIn(router)
+  const { googleSignIn, facebookSignIn, redirectToTop } =
+    useAuthThirdParty(router)
 
   const emailSignUp = async (values: { email: string; password: string }) => {
     try {
@@ -71,19 +73,52 @@ const SignUp: NextPage = () => {
           </Group>
         </form>
       </Box>
-      <div className='flex items-center my-20'>
+      <div className='flex items-center mt-10 mb-12'>
         <div className='grow border border-slate-500 border-solid'></div>
-        <span className='mx-6 text-lg text-slate-500'>OR</span>
+        <span className='mx-6 text-lg text-slate-300'>OR</span>
         <div className='grow border border-slate-500 border-solid'></div>
       </div>
-      <Button
-        onClick={googleSignIn}
-        size='md'
-        className='text-black bg-white'
-        leftIcon={<FcGoogle size={22} />}
-      >
-        Googleで登録
-      </Button>
+      <div className='mb-8 text-lg text-center'>お持ちのアカウントで登録</div>
+      <div className='flex justify-center items-center'>
+        <div className='flex flex-col items-center'>
+          <Image
+            src='/google-logo.png'
+            width='80px'
+            height='80px'
+            alt='google'
+            priority
+            onClick={googleSignIn}
+          />
+          <div className='mt-2'>Google</div>
+        </div>
+        <div className='flex flex-col items-center px-20'>
+          <Image
+            src='/facebook-logo.png'
+            width='80px'
+            height='80px'
+            alt='facebook'
+            priority
+            onClick={facebookSignIn}
+          />
+          <div className='mt-2'>Facebook</div>
+        </div>
+        <div className='flex flex-col items-center'>
+          <Image
+            src='/twitter-logo.png'
+            width='80px'
+            height='80px'
+            alt='twitter'
+            priority
+            onClick={googleSignIn}
+          />
+          <div className='mt-2'>Twitter</div>
+        </div>
+      </div>
+      <Link href='/signin'>
+        <a className='block mt-12 mr-8 text-lg text-right text-slate-300'>
+          登録済みの方はこちら
+        </a>
+      </Link>
     </div>
   )
 }
