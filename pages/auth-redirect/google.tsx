81b966c2988e284ba0a12ab37483af646f65e789
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-22 13:37:31 +0900

    💩 Google,Twitter,GitHubリダイレクト認証が無限ループ

--- pages/auth-redirect/google.tsx
+++ pages/auth-redirect/google.tsx
@@ -1,26 +1,29 @@
 import { LoadingOverlay } from '@mantine/core'
-import {
-  getRedirectResult,
-  signInWithRedirect,
-  UserCredential,
-} from 'firebase/auth'
+import { getRedirectResult, signInWithRedirect } from 'firebase/auth'
 import { auth, googleProvider } from 'firebaseConfig/firebase'
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect } from 'react'
+import { useEffect } from 'react'
 
 const AuthRedirectWithGoogle: NextPage = () => {
   const router = useRouter()
 
-  const redirectToMypage = useCallback(async (): Promise<void> => {
+  const redirectToMypage = (): void => {
     try {
-      const result: UserCredential | null = await getRedirectResult(auth)
-      if (result) {
-        const user = result.user
-        router.push(`/my-page/${user.uid}`)
-      } else {
-        signInWithRedirect(auth, googleProvider)
-      }
+      getRedirectResult(auth).then((result) => {
+        // getRedirectResultが呼ばれたとき
+        console.log('Called!')
+        if (result) {
+          // resultに値がある時
+          console.log('User.')
+          const user = result.user
+          router.push(`/my-page/${user.uid}`)
+        } else {
+          // resultがnullの時
+          console.log('Null')
+          signInWithRedirect(auth, googleProvider)
+        }
+      })
     } catch (error) {
       if (error instanceof Error) {
         const errorMessage = error.message
@@ -28,14 +31,14 @@ const AuthRedirectWithGoogle: NextPage = () => {
         router.push('/login')
       }
     }
-  }, [router])
+  }
 
   useEffect(() => {
     if (!router.isReady) {
       return
     }
     redirectToMypage()
-  }, [router.isReady, redirectToMypage])
+  }, [])
 
   return <LoadingOverlay visible={true} loaderProps={{ size: 'xl' }} />
 }
