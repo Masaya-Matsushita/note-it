fafb359552539668925bae8421be3c35f577f904
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-24 22:27:02 +0900

    ✨ redirect状態でredirectOnAuthStateを管理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,30 @@
 import { Layout } from 'components/Layout'
-import { useCheckIsSignIn } from 'hooks/useCheckIsSignIn'
+import { useRedirectOnAuthState } from 'hooks/useRedirectOnAuthState'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import 'styles/globals.css'
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
   const router = useRouter()
-  const checkIsSignIn = useCheckIsSignIn(router)
+  const [redirect, setRedirect] = useState(true)
+  const redirectOnAuthState = useRedirectOnAuthState(router, redirect)
 
   useEffect(() => {
-    console.log('useEffect is called')
-    checkIsSignIn()
-  }, [])
+    if (redirect) {
+      redirectOnAuthState()
+    }
+  }, [redirect])
 
   return (
     <>
       <Layout>
-        <Component {...pageProps} />
+        <Component
+          {...pageProps}
+          redirect={redirect}
+          setRedirect={setRedirect}
+        />
       </Layout>
     </>
   )
