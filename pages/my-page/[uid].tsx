5a8a5dbdfb973b29cc58d464273ad868881f23c8
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-26 01:02:05 +0900

    ✨ メール認証状態によって表示を変更

--- pages/my-page/[uid].tsx
+++ pages/my-page/[uid].tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react'
 const Mypage: NextPage = () => {
   const router = useRouter()
   const [loading, setLoading] = useState(false)
+  const [barrier, setBarrier] = useState(true)
 
   const signout = async (): Promise<void> => {
     setLoading(true)
@@ -25,6 +26,7 @@ const Mypage: NextPage = () => {
       ) {
         // 未認証
         // 確認メール再送信 & ログアウト用のモーダルを作成し、認証後再ログインしてもらう
+        setBarrier(true)
       } else {
         // 認証済み
         // コンテンツのfetch＆表示
@@ -34,10 +36,30 @@ const Mypage: NextPage = () => {
 
   return (
     <div>
-      <div>{router.query.uid}</div>
-      <Button onClick={signout} loading={loading}>
-        サインアウト
-      </Button>
+      {barrier ? (
+        <div>
+          <div>メールアドレスが未認証です</div>
+          <div>1.以下の「認証メールを送信」ボタンを押す。</div>
+          <div>
+            2.登録したメールアドレス宛に「認証メール」が届いていることを確認。
+          </div>
+          <div>
+            3.メール本文内のリンクをクリックすることで認証が完了します。
+          </div>
+          <div>
+            4.このページをリロードするか、以下の「ログイン画面へ」ボタンからログインし直してください。
+          </div>
+          <Button>認証メールを送信</Button>
+          <Button>ログイン画面へ</Button>
+        </div>
+      ) : (
+        <div>
+          <div>{router.query.uid}</div>
+          <Button onClick={signout} loading={loading}>
+            サインアウト
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
