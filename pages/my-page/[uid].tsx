de75823e5ffb0e2fea609712e1286ffaa929fb5a
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-06-26 02:13:58 +0900

    ✨ 確認メール送信とログアウトの処理を実装

--- pages/my-page/[uid].tsx
+++ pages/my-page/[uid].tsx
@@ -1,9 +1,16 @@
 import { Button } from '@mantine/core'
-import { signOut } from 'firebase/auth'
+import { showNotification } from '@mantine/notifications'
+import { SendEmailTroubleModal } from 'components/SendEmailTroubleModal'
+import {
+  onAuthStateChanged,
+  sendEmailVerification,
+  signOut,
+} from 'firebase/auth'
 import { auth } from 'firebaseConfig/firebase'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
 
 const Mypage: NextPage = () => {
   const router = useRouter()
@@ -17,21 +24,43 @@ const Mypage: NextPage = () => {
     router.push('/login')
   }
 
-  useEffect(() => {
+  const handleSendEmail = async (): Promise<void> => {
     const user = auth.currentUser
     if (user) {
-      if (
-        user.providerData[0].providerId === 'password' &&
-        user.emailVerified === false
-      ) {
-        // 未認証
-        // 確認メール再送信 & ログアウト用のモーダルを作成し、認証後再ログインしてもらう
-        setBarrier(true)
-      } else {
-        // 認証済み
-        // コンテンツのfetch＆表示
+      console.log('hoge')
+      auth.languageCode = 'ja'
+      const actionCodeSettings = {
+        url: 'https://note-it-five.vercel.app/mypage/' + user.uid,
       }
+      await sendEmailVerification(user, actionCodeSettings)
+      showNotification({
+        title: '送信完了！',
+        message: 'メールが届いていることをご確認ください。',
+        autoClose: 10000,
+        icon: <AiOutlineMail size={20} />,
+        style: { padding: '15px' },
+      })
     }
+  }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        if (
+          user.providerData[0].providerId === 'password' &&
+          user.emailVerified === false
+        ) {
+          // 未認証
+          // 確認メール再送信 & ログアウト用のモーダルを作成し、認証後再ログインしてもらう
+          console.log('ng')
+          setBarrier(true)
+        } else {
+          // 認証済み
+          // コンテンツのfetch＆表示
+          console.log('ok')
+        }
+      }
+    })
   }, [])
 
   return (
@@ -49,8 +78,11 @@ const Mypage: NextPage = () => {
           <div>
             4.このページをリロードするか、以下の「ログイン画面へ」ボタンからログインし直してください。
           </div>
-          <Button>認証メールを送信</Button>
-          <Button>ログイン画面へ</Button>
+          <Button onClick={handleSendEmail}>認証メールを送信</Button>
+          <Button onClick={signout} loading={loading}>
+            ログイン画面へ
+          </Button>
+          <SendEmailTroubleModal />
         </div>
       ) : (
         <div>
