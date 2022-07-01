6998d20530605a38a93faa83408625540512ed8c
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-07-01 23:53:56 +0900

    ✨ HeaderにPulldownMenuを追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
+import { Menu } from '@mantine/core'
 import { onAuthStateChanged } from 'firebase/auth'
 import { doc, getDoc } from 'firebase/firestore'
 import db, { auth } from 'firebaseConfig/firebase'
-import {useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import { AiOutlineDown } from 'react-icons/ai'
+import { Logout, Settings } from 'tabler-icons-react'
 
 export const Header = () => {
   const [userIcon, setUserIcon] = useState('')
@@ -27,11 +30,24 @@ export const Header = () => {
     <div className='flex justify-between p-4 bg-dark-700'>
       <div className='text-2xl'>Note It</div>
       {userIcon ? (
-        <img
-          src={userIcon}
-          alt='userIcon'
-          className='w-10 h-10 rounded-full sm:w-12 sm:h-12'
-        />
+        <Menu
+          control={
+            <div className='flex items-center'>
+              <img
+                src={userIcon}
+                alt='userIcon'
+                className='mr-1 w-10 h-10 rounded-full sm:w-12 sm:h-12'
+              />
+              <AiOutlineDown />
+            </div>
+          }
+        >
+          <Menu.Label>Menu</Menu.Label>
+          <Menu.Item icon={<Settings size={14} />}>Settings</Menu.Item>
+          <Menu.Item color='red' icon={<Logout size={14} />}>
+            Logout
+          </Menu.Item>
+        </Menu>
       ) : null}
     </div>
   )
