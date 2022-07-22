af129982950afed44964f2a52ca712876c32b13a
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-07-22 17:27:00 +0900

    🔥 clozeListを削除

--- pages/my-page/[uid]/[bookId]/note-form.tsx
+++ pages/my-page/[uid]/[bookId]/note-form.tsx
@@ -22,7 +22,7 @@ import { Book } from 'types'
 const NoteForm: NextPage = () => {
   const router = useRouter()
   const [targetBook, setTargetBook] = useState<Book | undefined>()
-  const [opened, setOpened] = useState(true)
+  const [opened, setOpened] = useState(false)
   const [popover, setPopover] = useState(false)
   const [label, setLabel] = useState('')
   const [page, setPage] = useState(0)
@@ -30,13 +30,6 @@ const NoteForm: NextPage = () => {
   const [cloze, setCloze] = useState(false)
   const [showClozeNote, setShowClozeNote] = useState(false)
   const [clozeNote, setClozeNote] = useState('')
-  const [clozeList, setClozeList] = useState<
-    {
-      start: number
-      range: number
-    }[]
-  >([])
-  const [clozeListIndex, setClozeListIndex] = useState(0)
 
   //マウント時にsessionStorageからデータを取得
   useEffect(() => {
@@ -65,7 +58,7 @@ const NoteForm: NextPage = () => {
           label: String(label),
           page: Number(page),
           note: String(note),
-          cloze: Boolean(cloze)
+          clozeNote: String(clozeNote),
         }
       )
       // 登録後、ページ遷移
@@ -78,66 +71,45 @@ const NoteForm: NextPage = () => {
     }
   }
 
+  // clozeが切り替わったとき、clozeNoteを変更
   useEffect(() => {
     if (cloze) {
       setClozeNote(note)
     } else {
       setClozeNote('')
-      setClozeList([])
-      setClozeListIndex(0)
     }
   }, [cloze])
 
-  const resetClozeList = () => {
-    if (clozeList.length) {
-      setClozeNote(note)
-      setClozeList([])
-      setClozeListIndex(0)
-    }
-  }
-
-  const pushClozeList = () => {
+  // 選択範囲を括弧抜きする
+  const updateClozeNote = () => {
     try {
-      const selectedRange = window.getSelection()?.getRangeAt(0)
-      if (selectedRange) {
-        setClozeList((prev) => {
-          return [
-            ...prev,
-            {
-              start: selectedRange.startOffset,
-              range: selectedRange.endOffset - selectedRange.startOffset,
-            },
-          ]
-        })
-        setClozeListIndex(clozeList.length)
+      const selectObj = window.getSelection()?.getRangeAt(0)
+      if (selectObj) {
+        // 選択範囲を取得
+        const selectStart = selectObj.startOffset
+        const selectRange = selectObj.endOffset - selectObj.startOffset
+        // 括弧を作成
+        const blank = Array(selectRange)
+        blank.fill('＿')
+        // 選択範囲を括弧へ置き換え
+        let clozeNoteArr = clozeNote.split('')
+        clozeNoteArr.splice(
+          selectStart,
+          selectRange,
+          ' ',
+          '(',
+          ' ',
+          blank.join(''),
+          ' ',
+          ')',
+          ' '
+        )
+        setClozeNote(clozeNoteArr.join(''))
       }
     } catch (error) {
+      // 範囲選択されていないとき
       console.error(error)
     }
-  }
-
-  useEffect(() => {
-    if (clozeList.length) {
-      updateClozeNote()
-    }
-  }, [clozeList])
-
-  const updateClozeNote = () => {
-    let clozeNoteArr = clozeNote.split('')
-    const blank = Array(clozeList[clozeListIndex].range)
-    blank.fill('＿')
-    clozeNoteArr.splice(
-      clozeList[clozeListIndex].start,
-      clozeList[clozeListIndex].range,
-      ' ',
-      '(',
-      ' ',
-      blank.join(''),
-      ' ',
-      ')',
-      ' '
-    )
-    setClozeNote(clozeNoteArr.join(''))
   }
 
   return (
@@ -154,14 +126,16 @@ const NoteForm: NextPage = () => {
         <div className='grow mb-2 border border-dark-400 border-solid'></div>
         <div className='p-2'>
           <div className=' mt-[3px] text-dark-300'>
-            追加後はnoteの編集が行えません。先にnoteの編集を完了させてください。
+            追加後はNoteの編集が行えません。先にNoteの編集を完了させてください。
           </div>
           <Popover
             opened={popover}
             onClose={() => setPopover(false)}
+            trapFocus={false}
             target={
               <div
-                onClick={() => setPopover((prev) => !prev)}
+                onMouseEnter={() => setPopover(true)}
+                onMouseLeave={() => setPopover(false)}
                 className='mb-2 text-dark-300 underline hover:cursor-pointer'
               >
                 ？ 追加方法
@@ -170,34 +144,37 @@ const NoteForm: NextPage = () => {
             width={260}
             placement='end'
             position='top'
-            className='block ml-[150px] text-right xxs:ml-40 xs:ml-72'
-            classNames={{popover: 'border-dark-500 border-solid'}}
+            className='block  text-right  xs:ml-72'
+            classNames={{
+              popover: 'border-dark-500 border-solid',
+              target: 'w-24 ml-auto',
+            }}
             withArrow
           >
             <Stepper orientation='vertical' active={0}>
-              <Stepper.Step label='note の括弧抜きする範囲を選択' />
-              <Stepper.Step label='「括弧抜きする」をクリック' />
+              <Stepper.Step label='Note の括弧抜きする範囲を選択' />
+              <Stepper.Step label='「選択範囲を括弧抜き」をクリック' />
               <Stepper.Step label='複数作成したい場合は１, 2 を繰り返し' />
             </Stepper>
           </Popover>
           <div className='ml-2 text-dark-200'>Note</div>
           <Card className='mb-4 min-h-[120px]'>{clozeNote}</Card>
-          <div className='flex justify-center mb-12'>
+          <div className='flex items-center mb-12'>
             <Button
               leftIcon={<BsArrowCounterclockwise />}
-              onClick={() => resetClozeList()}
-              className='mr-2 text-dark-100 bg-dark-500 hover:bg-dark-600'
+              onClick={() => setClozeNote(note)}
+              className='flex-1 mx-2 text-dark-100 bg-dark-500 hover:bg-dark-600'
             >
               リセット
             </Button>
             <Button
-              onClick={() => pushClozeList()}
-              className='ml-2 text-dark-100 bg-dark-500 hover:bg-dark-600'
+              onClick={() => updateClozeNote()}
+              className='flex-1 mx-2 text-dark-100 bg-dark-500 hover:bg-dark-600'
             >
-              括弧抜きする
+              選択範囲を括弧抜き
             </Button>
           </div>
-          <div className='flex justify-between'>
+          <div className='flex items-center'>
             <Button
               color='red'
               onClick={() => {
@@ -216,19 +193,21 @@ const NoteForm: NextPage = () => {
               }}
               className='flex-1 mr-2'
             >
-              OK
+              追加
             </Button>
           </div>
         </div>
       </Modal>
       <div className='ml-2 max-w-lg text-3xl'>Note作成</div>
-      <div className='mt-2 ml-4 text-dark-400'>- {targetBook?.title}</div>
+      <div className='mt-2 ml-4 text-lg text-dark-400'>
+        - {targetBook?.title}
+      </div>
       <div className='py-8 px-4 mt-6 mb-8 rounded-md border-dark-600 border-solid xs:px-6'>
         <div className='flex mr-4'>
           <TextInput
             required
             label='Label'
-            placeholder='ラベル(必須)'
+            placeholder='（ 例 ）炭酸水素ナトリウムの熱分解'
             size='md'
             value={label}
             onChange={(e) => setLabel(e.target.value)}
@@ -239,7 +218,7 @@ const NoteForm: NextPage = () => {
             min={0}
             required
             label='Page'
-            placeholder='0'
+            placeholder='26'
             size='md'
             value={page}
             onChange={(num) => (num ? setPage(num) : null)}
@@ -249,7 +228,7 @@ const NoteForm: NextPage = () => {
         <Textarea
           required
           label='Note'
-          placeholder='必須'
+          placeholder='炭酸水素ナトリウムを試験管に入れ、ガスバーナーで加熱すると、水と二酸化炭素が発生し、炭酸ナトリウムが残る。'
           size='md'
           value={note}
           onChange={(e) => setNote(e.target.value)}
@@ -257,7 +236,7 @@ const NoteForm: NextPage = () => {
           disabled={cloze}
           description={
             cloze
-              ? 'もう一度編集する場合は、「括弧抜きを追加」をoffにしてください。'
+              ? '編集する場合は「括弧抜きを追加」をoffにしてください。'
               : ''
           }
           className='mt-4'
