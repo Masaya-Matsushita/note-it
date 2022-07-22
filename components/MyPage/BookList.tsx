17e91e896612c0859e030b115020e9d1ef05f356
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-07-22 20:14:54 +0900

    ✨ bookListに編集・削除のメニューを作成

--- components/MyPage/BookList.tsx
+++ components/MyPage/BookList.tsx
@@ -1,7 +1,8 @@
-import { Accordion, Card } from '@mantine/core'
+import { Accordion, Menu } from '@mantine/core'
 import { auth } from 'firebaseConfig/firebase'
 import { NextRouter } from 'next/router'
 import { FC } from 'react'
+import { Pencil, Trash } from 'tabler-icons-react'
 import { Book, BadgeAndBooksList } from 'types'
 
 type Props = {
@@ -43,23 +44,41 @@ export const BookList: FC<Props> = ({ badgeAndBooksList, router }) => {
               <Accordion.Item label={badgeAndBooks.badge}>
                 {badgeAndBooks.books.map((book) => {
                   return (
-                    <Card
-                      className='p-4 mt-4 hover:bg-dark-600 hover:cursor-pointer'
+                    <div
                       key={book.id}
-                      onClick={() => {
-                        const targetBook = {
-                          badge: badgeAndBooks.badge,
-                          bookId: book.id,
-                          title: book.title,
-                          overview: book.overview,
-                        }
-                        toBookPage(targetBook)
-                      }}
+                      className='flex justify-between items-start mt-4 bg-dark-700 hover:bg-dark-600 rounded-lg hover:cursor-pointer'
                     >
-                      <div className='text-lg md:ml-2 md:text-xl'>
+                      <div
+                        className='flex-1 p-4 text-lg md:ml-2 md:text-xl'
+                        onClick={() => {
+                          const targetBook = {
+                            badge: badgeAndBooks.badge,
+                            bookId: book.id,
+                            title: book.title,
+                            overview: book.overview,
+                          }
+                          toBookPage(targetBook)
+                        }}
+                      >
                         {book.title}
                       </div>
-                    </Card>
+                      <Menu
+                        control={<div>...</div>}
+                        classNames={{
+                          root: 'w-12 text-center h-14 text-dark-400 text-4xl mr-4',
+                        }}
+                      >
+                        <Menu.Label>
+                          {book.title.length > 20
+                            ? book.title.slice(0, 20) + '...'
+                            : book.title}
+                        </Menu.Label>
+                        <Menu.Item icon={<Pencil size={14} />}>編集</Menu.Item>
+                        <Menu.Item color='red' icon={<Trash size={14} />}>
+                          削除
+                        </Menu.Item>
+                      </Menu>
+                    </div>
                   )
                 })}
               </Accordion.Item>
