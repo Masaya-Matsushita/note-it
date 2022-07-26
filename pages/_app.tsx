044847c2557cf4a50853d12ea71d711a8ffd9924
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-07-26 12:07:30 +0900

    🔥 useRedirectOnAutnStateファイルを削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,54 @@
 import { Layout } from 'components/Layout'
 import { Provider } from 'components/Layout/Provider'
-import { useRedirectOnAuthState } from 'hooks/useRedirectOnAuthState'
 import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
 import 'styles/globals.css'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from 'firebaseConfig/firebase'
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props
   const router = useRouter()
-  const redirectOnAuthState = useRedirectOnAuthState(router)
 
   // マウント時に一度だけ実行
   useEffect(() => {
     redirectOnAuthState()
   }, [])
+
+  // 未ログインのときloginへ、ログイン済のときmy-pageへ
+  const redirectOnAuthState = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        switch (router.pathname) {
+          case '/login':
+          case '/forgot-password':
+          case '/auth-redirect': {
+            router.push(`/my-page/${user.uid}`)
+            break
+          }
+          default: {
+            break
+          }
+        }
+      } else {
+        switch (router.pathname) {
+          case '/no-verified':
+          case '/my-page/[uid]':
+          case '/my-page/[uid]/book-form':
+          case '/my-page/[uid]/[bookId]':
+          case '/my-page/[uid]/[bookId]/note-form':
+          case '/my-page/[uid]/[bookId]/[noteId]': {
+            router.push('/login')
+            break
+          }
+          default: {
+            break
+          }
+        }
+      }
+    })
+  }
 
   return (
     <>
