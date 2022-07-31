892611567522d962ff2227fd214f7b1a4c7898d1
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-01 02:24:05 +0900

    🔥 不要なコードを削除

--- components/Parts/UserProfileModal/index.tsx
+++ components/Parts/UserProfileModal/index.tsx
@@ -10,14 +10,9 @@ import {
 import { onAuthStateChanged } from 'firebase/auth'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 import db, { auth, storage } from 'firebaseConfig/firebase'
-import {
-  getDownloadURL,
-  ref,
-  uploadBytes,
-  uploadString,
-} from 'firebase/storage'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
 import Cropper, { Area, MediaSize } from 'react-easy-crop'
-import { Button, Card, Modal, Skeleton, Slider, TextInput } from '@mantine/core'
+import { Button, Card, Modal, Slider, TextInput } from '@mantine/core'
 import { Plus } from 'tabler-icons-react'
 import { useRouter } from 'next/router'
 import { Reducer, useReducer } from 'react'
@@ -30,14 +25,14 @@ type Props = {
 type State = typeof initialState
 
 type Action = {
-  type: 'icon' | 'name' | 'error' | 'display' | 'loading'
+  type: 'icon' | 'name' | 'error' | 'display' | 'trimOpened' | 'closeTrim'
 } & Partial<State>
 
 const initialState = {
   error: '',
   iconURL: '',
   userName: '',
-  loading: true,
+  trimOpened: false,
 }
 
 const reducer: Reducer<State, Action> = (state, action) => {
@@ -46,7 +41,6 @@ const reducer: Reducer<State, Action> = (state, action) => {
       return {
         ...state,
         iconURL: action.iconURL ?? '',
-        loading: false,
       }
     }
     case 'name': {
@@ -60,20 +54,25 @@ const reducer: Reducer<State, Action> = (state, action) => {
         ...state,
         iconURL: action.iconURL ?? '',
         userName: action.userName ?? '',
-        loading: false,
       }
     }
     case 'error': {
       return {
         ...state,
         error: action.error ?? '',
-        loading: false,
       }
     }
-    case 'loading': {
+    case 'trimOpened': {
       return {
         ...state,
-        loading: action.loading ?? false,
+        iconURL: action.iconURL ?? '',
+        trimOpened: true,
+      }
+    }
+    case 'closeTrim': {
+      return {
+        ...state,
+        trimOpened: false,
       }
     }
   }
@@ -119,13 +118,8 @@ const getCroppedImg = async (
   // 抽出した画像データをcanvasの左隅に貼り付け
   ctx.putImageData(data, 0, 0)
 
-  // canvasを画像に変換
-  // return new Promise((resolve, reject) => {
-  // canvas.toBlob((file) => {
-  //   if (file !== null) resolve(URL.createObjectURL(file))
-  // }, 'image/jpeg')
+  // canvasを画像(Base64)に変換
   return canvas.toDataURL('image/jpeg')
-  // })
 }
 
 // eslint-disable-next-line react/display-name
@@ -133,27 +127,19 @@ export const UserProfileModal: FC<Props> = memo(({ opened, propsDispatch }) => {
   const router = useRouter()
   const uid = String(router.query.uid)
   const [state, dispatch] = useReducer(reducer, initialState)
-  const [trimOpened, setTrimOpened] = useState(false)
-  const [imgSrc, setImgSrc] = useState('/UnknownIcon.png')
   const [crop, setCrop] = useState({ x: 0, y: 0 })
   const [zoom, setZoom] = useState(1)
   const [minZoom, setMinZoom] = useState(1)
   const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>()
   const [croppedImgSrc, setCroppedImgSrc] = useState('')
 
-  // const stringToBlob = (str: string) => {
-  //   return new Blob([str], { type: 'text/plain' })
-  // }
-
   // userのドキュメントを作成/更新、リロード
   const setUserProfile = useCallback(async () => {
     if (state.userName && state.userName.length <= 20) {
-      // const blobCroppedImgSrc = stringToBlob(croppedImgSrc)
+      // Storageへアイコン画像(Base64)を保存
       const iconUsersRef = ref(storage, `users/${uid}/icon`)
-      // const fileCroppedImgSrc = new File([blobCroppedImgSrc], 'name', {
-      //   lastModified: 0,
-      // })
       await uploadString(iconUsersRef, croppedImgSrc, 'data_url')
+      // 保存した画像のパスを取得、ユーザーネームと共にFirestoreへ保存
       const iconUrl = await getDownloadURL(iconUsersRef)
       await setDoc(doc(db, 'users', uid), {
         userName: state.userName,
@@ -188,43 +174,28 @@ export const UserProfileModal: FC<Props> = memo(({ opened, propsDispatch }) => {
     })
   }, [])
 
-  // アイコンをアップロード＆URLを取得して表示
-  // const changeUserImage = useCallback(
-  //   async (e: ChangeEvent<HTMLInputElement>) => {
-  //     // fileが選択されてない場合
-  //     if (!e.target.files) {
-  //       return
-  //     }
-  //     dispatch({ type: 'loading', loading: true })
-  //     const file = e.target.files[0]
-  //     const iconUsersRef = ref(storage, `users/${uid}/icon`)
-  //     try {
-  //       await uploadBytes(iconUsersRef, file)
-  //       const iconUrl = await getDownloadURL(iconUsersRef)
-  //       dispatch({ type: 'icon', iconURL: iconUrl })
-  //     } catch (error: any) {
-  //       dispatch({ type: 'error', error: error.message })
-  //     }
-  //   },
-  //   [uid]
-  // )
-
   const ASPECT_RATIO = 1
   const CROP_WIDTH = 200
 
   // 画像ファイルをアップロードしてモーダルに表示
-  const onFileChange = useCallback(async (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const reader = new FileReader()
-      reader.addEventListener('load', () => {
-        if (reader.result) {
-          setImgSrc(reader.result.toString() || '')
-          setTrimOpened(true)
-        }
-      })
-      reader.readAsDataURL(e.target.files[0])
-    }
-  }, [])
+  const onFileChange = useCallback(
+    async (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files && e.target.files.length > 0) {
+        const reader = new FileReader()
+        reader.addEventListener('load', () => {
+          if (reader.result) {
+            dispatch({
+              type: 'trimOpened',
+              iconURL: reader.result.toString() || '',
+              trimOpened: true,
+            })
+          }
+        })
+        reader.readAsDataURL(e.target.files[0])
+      }
+    },
+    [propsDispatch]
+  )
 
   // 画像のZoomデフォルト値を設定
   const onMediaLoaded = useCallback((mediaSize: MediaSize) => {
@@ -257,13 +228,14 @@ export const UserProfileModal: FC<Props> = memo(({ opened, propsDispatch }) => {
   const showCroppedImage = useCallback(async () => {
     if (!croppedAreaPixels) return
     try {
-      const croppedImage = await getCroppedImg(imgSrc, croppedAreaPixels)
+      const croppedImage = await getCroppedImg(state.iconURL, croppedAreaPixels)
       setCroppedImgSrc(croppedImage)
-      setTrimOpened(false)
+      // setTrimOpened(false)
+      dispatch({ type: 'closeTrim' })
     } catch (e) {
       console.error(e)
     }
-  }, [croppedAreaPixels, imgSrc])
+  }, [croppedAreaPixels, propsDispatch, state.iconURL])
 
   return (
     <Modal
@@ -278,82 +250,88 @@ export const UserProfileModal: FC<Props> = memo(({ opened, propsDispatch }) => {
         アイコンと名前を設定してください。
       </div>
       <Card className='flex flex-col items-center mx-16'>
-        {state.loading ? (
-          <Skeleton className='w-20 h-20 rounded-full sm:w-24 sm:h-24' />
-        ) : (
-          <div>
-            <Modal
-              opened={trimOpened}
-              onClose={() => setTrimOpened(false)}
-              closeOnClickOutside={false}
-              closeOnEscape={false}
-              withCloseButton={false}
-            >
-              <div className='relative h-[220px] bg-gray-100'>
-                <Cropper
-                  image={imgSrc}
-                  crop={crop}
-                  zoom={zoom}
-                  minZoom={minZoom}
-                  maxZoom={minZoom + 3}
-                  aspect={ASPECT_RATIO}
-                  onCropChange={setCrop}
-                  onCropComplete={onCropComplete}
-                  onZoomChange={setZoom}
-                  cropSize={{
-                    width: CROP_WIDTH,
-                    height: CROP_WIDTH / ASPECT_RATIO,
-                  }}
-                  onMediaLoaded={onMediaLoaded}
-                  showGrid={false}
-                  cropShape='round'
-                />
-              </div>
-              <div className='mx-8 mt-4'>
-                <div className='ml-1'>Zoom</div>
-                <Slider
-                  size='lg'
-                  value={zoom}
-                  onChange={setZoom}
-                  min={minZoom}
-                  max={minZoom + 3}
-                  step={0.1}
-                  label={null}
-                  marks={[
-                    { value: 1, label: '×1' },
-                    { value: 2.5, label: '×2.5' },
-                    { value: 4, label: '×4' },
-                  ]}
-                />
-              </div>
-              <div className='flex justify-around mt-12'>
-                <Button color='red' onClick={() => setTrimOpened(false)}>
-                  Cancel
-                </Button>
-                <Button onClick={showCroppedImage}>OK</Button>
-              </div>
-            </Modal>
-            <div className='relative'>
-              <img
-                src={croppedImgSrc ? croppedImgSrc : imgSrc}
-                alt='アイコンの描画に失敗しました。'
-                className='w-20 h-20 rounded-full sm:w-24 sm:h-24'
+        <div>
+          <Modal
+            opened={state.trimOpened}
+            onClose={() => dispatch({ type: 'closeTrim' })}
+            closeOnClickOutside={false}
+            closeOnEscape={false}
+            withCloseButton={false}
+            className='mx-2 mt-16'
+          >
+            <div className='relative h-[220px] bg-dark-100'>
+              <Cropper
+                image={state.iconURL}
+                crop={crop}
+                zoom={zoom}
+                minZoom={minZoom}
+                maxZoom={minZoom + 3}
+                aspect={ASPECT_RATIO}
+                onCropChange={setCrop}
+                onCropComplete={onCropComplete}
+                onZoomChange={setZoom}
+                cropSize={{
+                  width: CROP_WIDTH,
+                  height: CROP_WIDTH / ASPECT_RATIO,
+                }}
+                onMediaLoaded={onMediaLoaded}
+                showGrid={false}
+                cropShape='round'
               />
-              <label htmlFor='iconURL' className='absolute left-14 sm:left-16'>
-                <input
-                  id='iconURL'
-                  type='file'
-                  accept='image/*,.png,.jpg,.jpeg,.gif'
-                  onChange={(e) => onFileChange(e)}
-                  className='hidden'
-                />
-                <div className='flex justify-center items-center w-6 h-6 bg-blue-500 rounded-full sm:w-8 sm:h-8'>
-                  <Plus size={20} strokeWidth={3} color={'white'} />
-                </div>
-              </label>
             </div>
+            <div className='mx-8 mt-4'>
+              <div className='ml-1'>Zoom</div>
+              <Slider
+                size='lg'
+                value={zoom}
+                onChange={setZoom}
+                min={minZoom}
+                max={minZoom + 3}
+                step={0.1}
+                label={null}
+                marks={[
+                  { value: 0.9, label: '×1' },
+                  { value: 2.45, label: '×2.5' },
+                  { value: 3.9, label: '×4' },
+                ]}
+              />
+            </div>
+            <div className='flex justify-between mt-12'>
+              <Button
+                color='red'
+                onClick={() => dispatch({ type: 'closeTrim' })}
+                className='ml-4 w-28 h-10 xs:ml-8 xs:w-36'
+              >
+                Cancel
+              </Button>
+              <Button
+                onClick={showCroppedImage}
+                className='mr-4 w-28 h-10 xs:mr-8 xs:w-36'
+              >
+                OK
+              </Button>
+            </div>
+          </Modal>
+          <div className='relative'>
+            <img
+              src={croppedImgSrc ? croppedImgSrc : state.iconURL}
+              alt='アイコンの描画に失敗しました。'
+              className='w-20 h-20 rounded-full sm:w-24 sm:h-24'
+            />
+            <label htmlFor='iconURL' className='absolute left-14 sm:left-16'>
+              <input
+                id='iconURL'
+                type='file'
+                accept='image/*,.png,.jpg,.jpeg,.gif'
+                onChange={(e) => onFileChange(e)}
+                className='hidden'
+              />
+              <div className='flex justify-center items-center w-6 h-6 bg-blue-500 rounded-full sm:w-8 sm:h-8'>
+                <Plus size={20} strokeWidth={3} color={'white'} />
+              </div>
+            </label>
           </div>
-        )}
+        </div>
         <TextInput
           placeholder='User Name'
           value={state.userName}
