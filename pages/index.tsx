740288225c54fe719cda1ef1bbc42420085efaf3
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-03 22:49:22 +0900

    ✨ TOPページを微調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@mantine/core'
 import { DevideLabel } from 'components/Parts/DevideLabel'
 import { NextPage } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const ITEMS = [
   { id: '1', label: 'ジャンル別、本別で簡単整理' },
@@ -12,25 +13,35 @@ const ITEMS = [
 const Top: NextPage = () => {
   return (
     <>
-      <div className='absolute top-[500px] left-0 -z-10 w-screen h-[3000px]'>
+      <div className='absolute top-[480px] left-0 -z-10 w-screen md:hidden md:h-[1800px]'>
         <Image src='/BG-1.png' alt='' layout='fill' />
       </div>
-      <Button variant='outline' className='block mb-2 ml-auto w-28 xs:mb-12'>
-        ログイン
-      </Button>
-      <div className='flex flex-col xs: xs:flex-row'>
-        <div className='mb-6 text-center xs:mt-12 xs:w-48 sm:ml-12 md:ml-20 md:w-60'>
-          <div className='my-4 text-4xl xs:tracking-wide md:text-5xl'>
+      <div className='hidden absolute top-[480px] left-0 -z-10 w-screen md:block md:h-[1800px]'>
+        <Image src='/BG-2.png' alt='' layout='fill' />
+      </div>
+      <Link href='/login' passHref>
+        <Button
+          variant='outline'
+          className='block mb-2 ml-auto w-28 xs:mr-8 xs:mb-12 md:mr-12 md:w-36 md:h-10'
+        >
+          ログイン
+        </Button>
+      </Link>
+      <div className='flex flex-col xs:flex-row'>
+        <div className='mt-8 mb-6 text-center xs:mt-24 xs:w-48 sm:ml-12 md:ml-20 md:w-60'>
+          <div className='mb-2 text-4xl tracking-wider sm:mb-4 md:text-5xl'>
             Note It
           </div>
           <div className='xs:mb-16 xs:text-lg md:text-xl'>
             暗記特化型メモアプリ
           </div>
-          <Button variant='light' className='hidden w-40 h-10 xs:inline'>
-            使ってみる
-          </Button>
+          <Link href='/login' passHref>
+            <Button variant='light' className='hidden w-40 h-10 xs:inline'>
+              使ってみる
+            </Button>
+          </Link>
         </div>
-        <div className='relative mx-auto w-52 h-[430px] xxs:w-64 xxs:h-[500px]'>
+        <div className='relative mx-auto w-52 h-[410px] xxs:w-64 xxs:h-[500px]'>
           <Image
             src='/iphone-book1.png'
             alt='イメージ'
@@ -38,25 +49,38 @@ const Top: NextPage = () => {
             priority
           />
         </div>
-        <div className='mx-8 mt-8'>
-          <Button variant='light' className='w-full h-10 xs:hidden'>
+        <Link href='/login' passHref>
+          <Button
+            variant='light'
+            className='block mx-auto mt-16 w-40 h-10 xxs:w-48 xs:hidden'
+          >
             使ってみる
           </Button>
-        </div>
+        </Link>
       </div>
-      <h1 className='mt-32'>「ピンポイントで大事なところだけ」</h1>
-      <div className='mx-auto max-w-xl'>
+      <h1 className='mt-32 mb-8 text-dark-200 sm:ml-4 md:ml-20'>
+        「ピンポイントで大事なところだけ」
+      </h1>
+      <div className='mx-4 mb-40 max-w-2xl text-lg leading-7 text-dark-200 sm:mx-auto'>
         <div>
           受験勉強や資格の勉強、現代人は覚えることがたくさん。本も山積み。
         </div>
-        <ul>
-          <li>あの内容を見返したい！でも、どこに書いてあったっけ？</li>
-          <li>マーカーだらけで読みづらい...</li>
-        </ul>
-        <div>もっと賢く管理してみませんか？</div>
         <div>
-          Note
-          Itは「ピンポイントで大事なところだけ」括弧抜きで管理することで、あなたの復習を効率化、知識定着を支援するアプリです。
+          <div className='mt-4 ml-4 text-dark-300'>
+            - あの内容を見返したい！でも、どこに書いてあったっけ？
+          </div>
+          <div className='mb-6 ml-4 text-dark-300'>
+            - 付箋やマーカーだらけ、どこが大事か分かりづらい...
+          </div>
+        </div>
+        <div className='mb-1'>
+          もっと<span className='mx-1 font-bold text-blue-400'>賢く管理</span>
+          してみませんか？
+        </div>
+        <div>
+          Note It は「 ピンポイントで大事なところだけ 」
+          <span className='mx-1 font-bold text-blue-400'>括弧抜き</span>
+          で管理することで、あなたの復習を効率化、知識定着を支援するアプリです。
         </div>
       </div>
       <DevideLabel>
@@ -65,11 +89,11 @@ const Top: NextPage = () => {
         </span>
       </DevideLabel>
       <div className='text-center'>サービスの特徴</div>
-      <div className='flex flex-col items-center my-8 xs:flex-row xs:flex-wrap xs:justify-center'>
+      <div className='flex flex-col items-center my-12 xs:flex-row xs:flex-wrap xs:justify-center'>
         {ITEMS.map((item) => {
           return (
             <div key={item.id} className='my-8 mx-4'>
-              <div className='relative w-72 h-72 xs:w-[296px] xs:h-[296px]'>
+              <div className='relative w-72 h-72 xs:w-[282px] xs:h-[282px]'>
                 <div className='text-4xl font-bold text-dark-300'>
                   {item.id}
                 </div>
@@ -86,6 +110,14 @@ const Top: NextPage = () => {
           )
         })}
       </div>
+      <Link href='/login' passHref>
+        <Button
+          variant='light'
+          className='block mx-auto mt-48 mb-8 w-5/6 h-12 xs:w-96'
+        >
+          使ってみる ( ログイン画面へ )
+        </Button>
+      </Link>
     </>
   )
 }
